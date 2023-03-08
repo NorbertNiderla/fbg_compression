a8e3e96828967c09a3a8ca7ce59aaf0c8b16c80f
Norbert Niderla <norbert.niderla@gmail.com>
2023-03-08 21:48:36 +0100

    refactored program into nicer form

--- python/fbg_compression.py
+++ python/fbg_compression.py
@@ -10,130 +10,94 @@ from coders.bitstream import bitstream_get_bits
 from algorithms import algorithm_sprintz
 from denoise_data import denoise_data
 
-
 DATA_FILES_FOLDER = "C:/Users/norbert/PycharmProjects/data"
-MAX_SAMPLE_VALUE = 16384 - 1
 PEAK_DETECTION_BASIC_THRESHOLD = 1.1
-CONFIG = {
-    "algorithms": {
-        "Sprintz": {
-            "use": True,
-            "function": algorithm_sprintz,
-        }
-    },
-    "input": {
-        "raw": True,
-        "noiseless": True,
-        "peaks": {
-            "stream": True,
-            "xy": False
-        }
-    }
-}
+
+
+class AlgorithmResult:
+    def __init__(self, algorithm: str):
+        self.algorithm = algorithm
+        self.bits = []
+
+    def append_bits(self, bits: int):
+        self.bits.append(bits)
+
+    def print_results(self):
+        print(f"\t\t{self.algorithm}: {average(self.bits)}")
+
+
+class DataSetResults:
+    def __init__(self, data_set_name: str, original_bitwidth: int):
+        self.original_bitwidth = original_bitwidth
+        self.max_value = 2 ** self.original_bitwidth - 1
+        self.data_set_name = data_set_name
+        self.entropy = []
+        self.result_sprintz = AlgorithmResult("sprintz")
+
+    def process(self, data):
+        self.append_entropy(entropy_my(self.max_value, data))
+        self.result_sprintz.append_bits(algorithm_sprintz(data))
+
+    def append_entropy(self, val: float):
+        self.entropy.append(val)
+
+    def print_results(self):
+        print(f"\tData set: {self.data_set_name}")
+        print(f"\tOriginal bitwidth: {self.original_bitwidth}")
+        print(f"\tMax value: {self.max_value}")
+        print(f"\tEntropy: {average(self.entropy)}")
+        self.result_sprintz.print_results()
+
+
+class Results:
+    def __init__(self):
+        self.result_raw = DataSetResults("raw", 14)
+        self.result_noiseless = DataSetResults("noiseless", 14)
+        self.result_peaks_stream = DataSetResults("peaks stream", 12)
+
+    def print_results(self):
+        print("Results:")
+        self.result_raw.print_results()
+        self.result_noiseless.print_results()
+        self.result_peaks_stream.print_results()
 
 
 def main():
     print("Fbg Compression")
     print(f"\tData folder: {DATA_FILES_FOLDER}")
 
+    results = Results()
     file_step = 3000
     dense_file_step = int(ceiling(file_step / 30))
-
     fbg_data = FbgData(DATA_FILES_FOLDER, 3000)
     fbg_data_dense = FbgData(DATA_FILES_FOLDER, dense_file_step)
-
     n_f = fbg_data.get_number_of_files()
-    entropy_raw = [0] * n_f
-    entropy_noiseless = [0] * n_f
-    entropy_peaks_stream = 0
-    bits_raw = {key: [0] * n_f for key, value in CONFIG["algorithms"].items() if value["use"] is True}
-    bits_noiseless = {key: [0] * n_f for key, value in CONFIG["algorithms"].items() if value["use"] is True}
-    bits_peak_stream = {key: 0 for key, value in CONFIG["algorithms"].items() if value["use"] is True}
 
     print("Processing normal fbg data...")
     for i in tqdm(range(n_f)):
         data, last_file = fbg_data.get_data()
-
-        if CONFIG["input"]["raw"] is True:
-            data_raw = data[:(len(data) - (len(data) % 32))]
-            entropy_raw[i] = entropy_my(MAX_SAMPLE_VALUE, data_raw)
-            for key, value in CONFIG["algorithms"].items():
-                if value["use"] is True:
-                    bits_raw[key][i] = value["function"](data_raw)
-
-        if CONFIG["input"]["noiseless"] is True:
-            data_noiseless = [int(round(x)) for x in denoise_data(data)]
-            data_noiseless = data_noiseless[:(len(data_noiseless) - (len(data_noiseless) % 32))]
-            entropy_noiseless[i] = entropy_my(MAX_SAMPLE_VALUE, data_noiseless)
-            for key, value in CONFIG["algorithms"].items():
-                if value["use"] is True:
-                    bits_noiseless[key][i] = value["function"](data_noiseless)
+        data_raw = data[:(len(data) - (len(data) % 32))]
+        results.result_raw.process(data_raw)
+        data_noiseless = [int(round(x)) for x in denoise_data(data)]
+        data_noiseless = data_noiseless[:(len(data_noiseless) - (len(data_noiseless) % 32))]
+        results.result_noiseless.process(data_noiseless)
 
     print("Calculating peaks from dense fbg data...")
     n_f = fbg_data_dense.get_number_of_files()
     peaks_band_method = [0] * n_f
     for i in tqdm(range(n_f)):
         data, last_file = fbg_data_dense.get_data()
-        if CONFIG["input"]["peaks"]["stream"] is True:
-            peaks_band_method[i] = peak_detection_band(data)
+        peaks_band_method[i] = peak_detection_band(data)
 
     print("Processing peak data...")
-    number_of_peaks_in_stream = 0
-    if CONFIG["input"]["peaks"]["stream"] is True:
-        peaks_stream_x, peaks_stream_y = convert_peaks_into_stream(peaks_band_method)
-        scale = 2 ** 12 - 1
-        peaks_stream = [round(val * scale) for val in peaks_stream_y]
-        peaks_stream = peaks_stream[:len(peaks_stream) - len(peaks_stream) % 32]
-        for key, value in CONFIG["algorithms"].items():
-            if value["use"] is True:
-                bits_peak_stream[key] = value["function"](peaks_stream)
-        number_of_peaks_in_stream = len(peaks_stream)
-        entropy_peaks_stream = entropy_my(scale, peaks_stream)
-    original_bitwidth = 14
+    peaks_stream_x, peaks_stream_y = convert_peaks_into_stream(peaks_band_method)
+    scale = 2 ** 12 - 1
+    peaks_stream = [round(val * scale) for val in peaks_stream_y]
+    peaks_stream = peaks_stream[:len(peaks_stream) - len(peaks_stream) % 32]
+    results.result_peaks_stream.process(peaks_stream)
+    number_of_peaks_in_stream = len(peaks_stream)
 
-    print("Results:")
-    # Printing data from raw data processing
-    if CONFIG["input"]["raw"] is True:
-        print("\tRaw data lossless compression:")
-        print(f"\t\tOriginal bitwidth: {original_bitwidth}")
-        print(f"\t\tData entropy: {average(entropy_raw)}")
-        print("\t\tBits per sample in compressed stream:")
-        for key, value in CONFIG["algorithms"].items():
-            if value["use"] is True:
-                print(f"\t\t\t{key}: {average(bits_raw[key])}")
-
-    # Printing data from denoised data processing
-    if CONFIG["input"]["noiseless"] is True:
-        print("\tDenoised data lossless compression:")
-        print(f"\t\tOriginal bitwidth: {original_bitwidth}")
-        print(f"\t\tData entropy: {average(entropy_noiseless)}")
-        print(f"\t\tBits per sample in compressed stream:")
-        for key, value in CONFIG["algorithms"].items():
-            if value["use"] is True:
-                print(f"\t\t\t{key}: {average(bits_noiseless[key])}")
-
-    # Printing data from peaks stream processing
-    original_bitwidth = 12
-    if CONFIG["input"]["peaks"]["stream"] is True:
-        print("\tPeak stream lossless compression:")
-        print(f"\t\tNumber of peaks in peaks stream: {number_of_peaks_in_stream}")
-        print(f"\t\tOriginal bitwidth: {original_bitwidth}")
-        print(f"\t\tPeak stream entropy: {entropy_peaks_stream}")
-        print(f"\t\tBits per sample in compressed stream:")
-        for key, value in CONFIG["algorithms"].items():
-            if value["use"] is True:
-                print(f"\t\t\t{key}: {bits_peak_stream[key]}")
-
-    # fig, axs = plt.subplots(1)
-    # fig.suptitle('Lossless compression')
-    # axs.plot(entropy_all, label="entropy")
-    # axs.plot(bits_all, label="compression")
-    # axs.plot([original_bitwidth for _ in range(len(bits_all))], label="original")
-    # axs.set_title("Sprintz compression results")
-    # axs.grid(True)
-    # axs.set_ylim([0, original_bitwidth + 1])
-    # plt.legend()
-    # plt.show()
+    results.print_results()
 
 
 def entropy_my(max_value, labels, base=None):
@@ -192,47 +156,6 @@ def get_peaks_band_method(fbg_data_source):
         peak_band_all[x] = peak_detection_band(data)
         x += 1
     return peak_band_all
-
-
-def show_peaks_lossless_compression(fbg_data_source):
-    peak_band_all = get_peaks_band_method(fbg_data_source)
-    peak_band_all_xy = convert_peaks_into_xy(peak_band_all)
-    peaks_stream_x, peaks_stream_y = convert_peaks_into_stream(peak_band_all)
-
-    print("Peaks lossless compression:")
-    print(f"\tNumber of peaks detected: {len(peak_band_all_xy[0])}")
-    print(f"\tNumber of peaks detected per oscillogram: {len(peak_band_all_xy[0]) / fbg_data_source.get_number_of_files()}")
-    print(f"\tNumber of peaks in peaks stream: {len(peaks_stream_x)}")
-    print(f"\tNumber of peaks in peaks stream per oscillogram: {len(peaks_stream_x) / fbg_data_source.get_number_of_files()}")
-
-    # fig, axs = plt.subplots(1)
-    # fig.suptitle('Peaks lossless compression')
-    # axs.plot(peak_band_all_xy[0], peak_band_all_xy[1], "bo", label="peaks")
-    # axs.plot(peaks_stream_x, peaks_stream_y, label="peaks stream", color="r")
-    # axs.grid(True)
-    # axs.set_ylim([0, 1])
-    # plt.legend()
-    # plt.show()
-
-    scale = 2 ** 12 - 1
-    peaks_stream = [round(val * scale) for val in peaks_stream_y]
-    peaks_stream = peaks_stream[:len(peaks_stream) - len(peaks_stream) % 32]
-
-    # sprintz
-    fire_bitwidth = 16
-    fire_learn_shift = -1
-    compressor_fire = Fire(fire_bitwidth, fire_learn_shift)
-    decompressor_fire = Fire(fire_bitwidth, fire_learn_shift)
-    compressed_data = sprintz_encode(peaks_stream, compressor_fire, 32)
-    bits = bitstream_get_bits(compressed_data)
-    decompressed_data = sprintz_decode(compressed_data, decompressor_fire, 32)
-    if peaks_stream != decompressed_data:
-        raise ValueError("Sprintz coder failed!")
-
-    print(f"\tSprintz peaks lossless compression:")
-    print(f"\t\tOriginal bitwidth: {12}")
-    print(f"\t\tData entropy: {entropy_my(scale, peaks_stream)}")
-    print(f"\t\tBits per sample: {bits / len(peaks_stream)}")
 
 
 if __name__ == "__main__":
--- python/input_data_statistics.py
+++ python/input_data_statistics.py
@@ -2,11 +2,24 @@ from data import FbgData
 from peak_detection import peak_detection_max, peak_detection_band
 from fbg_compression import entropy_my, convert_None_to_NaN, convert_peaks_into_xy
 from numpy import convolve, ones, average
-from matplotlib import plot as plt
+from matplotlib import pyplot as plt
+from statistics import variance
+from numpy import diff
 
 DATA_FILES_FOLDER = "C:/Users/norbert/PycharmProjects/data"
 MAX_SAMPLE_VALUE = 16384 - 1
 PEAK_DETECTION_BASIC_THRESHOLD = 1.1
+
+
+def show_raw_figure(fbg_data_source: FbgData):
+    fig, axs = plt.subplots(1)
+    fig.suptitle('Raw Data')
+    data = fbg_data_source.get_data_with_index(round(fbg_data_source.get_number_of_files() / 2))
+    print(f"Variance of raw data: {variance(diff(data), 0)}")
+    axs.plot(data)
+    axs.set_title("Example oscillogram")
+    axs.grid(True)
+    axs.set_ylim([0, MAX_SAMPLE_VALUE])
 
 
 def show_average_figure(fbg_data_source: FbgData):
@@ -46,43 +59,51 @@ def show_statistics(fbg_data_source: FbgData):
     peak_max_arr = convert_None_to_NaN(peak_max_arr)
     peak_band_arr_x, peak_band_arr_y = convert_peaks_into_xy(peak_band_arr)
 
-    fig, axs = plt.subplots(5)
-    fig.suptitle('Input data statistics')
+    fig, axs = plt.subplots(2)
+    fig.suptitle('Input data entropy')
 
     a = 0
     axs[a].plot(entropy_arr)
     axs[a].set_title("Entropy")
     axs[a].grid(True)
     axs[a].set_ylim([2, 8])
+    plt.xlabel("Frame number")
+    plt.ylabel("Set entropy")
+
     a += 1
 
     axs[a].plot(moving_average_entropy_arr)
     axs[a].set_title("Moving Average Entropy")
     axs[a].grid(True)
     axs[a].set_ylim([2, 8])
+    # axs[a].xlabel("Frame number")
+    # axs[a].ylabel("Set entropy")
+
+
+    a = 0
+
+    fig2, axs2 = plt.subplots(2)
+    fig2.suptitle('Peak detection')
+    axs2[a].plot(peak_max_arr, "bo")
+    axs2[a].set_title("Peak Detection (Basic Method)")
+    axs2[a].grid(True)
+    axs2[a].set_ylim([0, 1])
+    # axs[a].ylabel("Normalized wavelength")
+    # axs[a].xlabel("Frame number")
     a += 1
 
-    axs[a].plot(peak_max_arr, "bo")
-    axs[a].set_title("Peak Detection (Basic Method)")
-    axs[a].grid(True)
-    axs[a].set_ylim([0, 1])
-    a += 1
-
-    axs[a].plot(peak_band_arr_x, peak_band_arr_y, "bo")
-    axs[a].set_title("Peak Detection (Band Method)")
-    axs[a].grid(True)
-    axs[a].set_ylim([0, 1])
-    a += 1
-
-    axs[a].plot(fbg_data_source.get_data_with_index(round(fbg_data_source.get_number_of_files() / 2)))
-    axs[a].set_title("Example oscillogram")
-    axs[a].grid(True)
-    axs[a].set_ylim([0, MAX_SAMPLE_VALUE])
+    axs2[a].plot(peak_band_arr_x, peak_band_arr_y, "bo")
+    axs2[a].set_title("Peak Detection (Band Method)")
+    axs2[a].grid(True)
+    axs2[a].set_ylim([0, 1])
+    # axs[a].ylabel("Normalized wavelength")
+    # axs[a].xlabel("Frame number")
     a += 1
 
 
 if __name__ == "__main__":
     fbg_data = FbgData(DATA_FILES_FOLDER, 500)
     show_statistics(fbg_data)
+    show_raw_figure(fbg_data)
     show_average_figure(fbg_data)
     plt.show()
